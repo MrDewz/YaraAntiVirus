97e2b42049fcb7118aaec107597313d3a0180a64
Dewz <Dewz@DESKTOP-D07LP9M>
2024-03-03 19:35:59 +0300

    Добавьте файлы проекта.

---
+++ AVFramework/MainWindow.xaml.cs
@@ -0,0 +1,158 @@
+﻿//using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using YaraSharp;
+
+namespace AVFramework
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static string BaseDirectory = Environment.CurrentDirectory;
+
+        private static List<string> ruleFilenames;
+
+        private static List<string> testfiles;
+
+        Dictionary<string, object> externals = new Dictionary<string, object>()
+            {
+                { "filename", string.Empty },
+                { "filepath", string.Empty },
+                { "extension", string.Empty }
+            };
+        YSInstance yaraInstance = new YSInstance();
+
+        YSContext context = new YSContext();
+
+        YSCompiler compiler = new YSCompiler(null);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ruleFilenames = Directory.GetFiles(Path.Combine(BaseDirectory, "Rules"), "*.yar", SearchOption.AllDirectories).ToList();
+            //testfiles = Directory.GetFiles(Path.Combine(BaseDirectory, "TestData"), "*.txt", SearchOption.AllDirectories).ToList();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (var openFolderDialog = new FolderBrowserDialog())
+                {
+                    DialogResult result = openFolderDialog.ShowDialog();
+
+                    if (result == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(openFolderDialog.SelectedPath))
+                    {
+                        testfiles = Directory.GetFiles(openFolderDialog.SelectedPath, "*", SearchOption.AllDirectories).ToList();
+                        System.Windows.MessageBox.Show("Files found: " + testfiles.Count.ToString(), "Message");
+                        DisplayRulesLoad();
+                        Scan(0);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                System.Windows.MessageBox.Show("Ошибка");
+                throw;
+            }
+
+        }
+
+        private void ChooseFileBtn_Click(object sender, RoutedEventArgs e)
+        { 
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    testfiles = openFileDialog.FileNames.ToList();
+                    ScanPG.Maximum = testfiles.Count;
+                    DisplayRulesLoad();
+                    Scan().Start();
+                }
+            }
+        }
+
+        private async Task Scan()
+        {
+            try
+            {
+                YaraScanner yaraScanner = new YaraScanner(compiler);
+                for (int i = 0; i < testfiles.Count; i++)
+                {
+                    CurrentTask.Text = $"Сейчас сканируется - {testfiles[i]}";
+                    //yaraScanner.ScanFile(testfiles[i]).Start();
+                    //System.Windows.MessageBox.Show($"File {testfiles[i]} is {yaraScanner.ScanFile(testfiles[i])} virus");
+                    //_i++;
+                    //ScanPG.Value += _i;
+
+                    //Scan(_i);                   
+                    yaraScanner.ScanFile(testfiles[i]).Wait();
+                    bool result = yaraScanner.ScanFile(testfiles[i]).Result;
+                    LogBox.AppendText($"File {testfiles[i]} is {result} virus\n");
+                    break;
+                }
+            }
+            catch (Exception)
+            {
+                System.Windows.MessageBox.Show("Ошибка сканирования");
+                throw;
+            }
+        }
+
+        private async Task LoadRules()
+        {
+            try
+            {
+                compiler = yaraInstance.CompileFromFiles(ruleFilenames, externals);
+            }
+            catch (Exception)
+            {
+                System.Windows.MessageBox.Show("Ошибка загрузки сигнатур");
+                throw;
+            }
+        }
+
+        private void DisplayRulesLoad()
+        {
+            string[] LoadingBar = { "[\\]", "[|]", "[/]", "[--]" };
+            int i = 0;
+            CurrentTask.Text = "Loading virus signature database, please wait...";
+            LoadRules();  
+            while (!LoadRules().IsCompleted)
+            {
+                CurrentTask.Text = "Loading virus signature database, please wait..." + LoadingBar[i];
+                i++;
+                if (i == 4)
+                {
+                    i = 0;
+                }
+            }
+        }
+    }
+}
---
+++ AVFramework/YaraScanner.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using YaraSharp;
+
+namespace AVFramework
+{
+    public class YaraScanner
+    {
+        private YSCompiler _compiler;
+        //private YSContext _context;
+        private YSInstance _yaraInstance = new YSInstance();
+
+        public YaraScanner(YSCompiler compiler)
+        {
+            _compiler = compiler;
+            //_compiler = _yaraInstance.CompileFromFiles(ruleFilenames, externals);
+            //LoadRules(ruleFiles);
+            //_context = new YSContext();
+        }
+
+        //public void LoadRules(string[] ruleFiles)
+        //{
+        //    foreach (var ruleFile in ruleFiles)
+        //    {
+        //        _compiler.AddFile(ruleFile);              
+        //    }
+        //}
+
+        public async Task<bool> ScanFile(string filePath)
+        {
+            //YSReport compilerErrors = _compiler.GetErrors();
+            //YSReport compilerWarnings = _compiler.GetWarnings();
+
+            YSScanner scanner = new YSScanner(_compiler.GetRules(), null);
+            //var f = scanner.ScanFile(filePath);
+            bool result = scanner.ScanFile(filePath).Any(r => r.Rule.Identifier == "WarningRule");
+            scanner.Dispose();
+            //_compiler.Dispose();
+            return result;
+          
+        }
+    }
+}
